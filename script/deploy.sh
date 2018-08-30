692d4d3c2b108791c47e12d83de583c8b0e9e63b
rsb007 <rahul.sing@knoldus.in>
2018-08-30 14:24:57 +0530

    play assignment

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,4 +1,5 @@
 rm -r play-final-assignment-1.0-SNAPSHOT | echo "artifact already deleted"
-unzip play-final-assignment-1.0-SNAPSHOT.zip  
+unzip play-final-assignment-1.0-SNAPSHOT.zip
+rm -f play-final-assignment-1.0-SNAPSHOT.zip  
 ./play-final-assignment-1.0-SNAPSHOT/bin/play-final-assignment &
 
